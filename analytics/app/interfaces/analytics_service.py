8f5ba1d8a1431de558075f142d401bfe1737cefb
NanoCortes <n20072701@gmail.com>
2026-04-09 06:39:19 -0500

    Limpieza de rama developer/Felipe: eliminando microservicios innecesarios

--- analytics/app/interfaces/analytics_service.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List, Dict
-
-class IAnalyticsService(ABC):
-    @abstractmethod
-    def get_zones(self, dataset_hash: str) -> List[Dict[str, str]]:
-        pass
--- recommendations/app/core/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    project_name: str = "Microservice"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
